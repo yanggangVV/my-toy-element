ce0f33a3086d2b756007755a4dff455dc807c86e
yg <791804350@qq.com>
2024-05-20 16:04:21 +0800

    fun:1、Button组件开发2、测试用例生成（用kimi）

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,8 +1,28 @@
 <script setup lang="ts">
+import { ref } from "vue"
+import type { ButtonProps } from "./types"
+
 defineOptions({
-  name:'YgButton'
+  name: 'YgButton'
 })
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: 'button',
+  nativeType: 'button',
+})
+const slots = defineSlots()
+const _ref = ref<HTMLButtonElement>()
 </script>
 <template>
-	<button style="background-color: blue;color: red;">this is a button</button>
+  <component :is="props.tag" ref="_ref" :type="tag === 'button' ? nativeType : void 0"
+    :disabled="disabled || loading ? true : void 0" :class="{
+      [`yg-button--${type}`]: type,
+      [`yg-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+    }">
+    <slot></slot>
+    </component>
 </template>
---
+++ packages/components/Button/types.ts
@@ -0,0 +1,21 @@
+import { type Component, type ComputedRef, type Ref } from "vue";
+export type ButtonType = "primary" | "success" | "warning" | "danger" | "info";
+export type NativeType = "button" | "submit" | "reset";
+export type ButtonSize = "default" | "large" | "small";
+
+export interface ButtonProps {
+  tag?: string | Component;
+  type?: ButtonType;
+  size?: ButtonSize;
+  plain?: boolean;
+  round?: boolean;
+  circle?: boolean;
+  disabled?: boolean;
+  autofocus?: boolean;
+  nativeType?: NativeType;
+  icon?: string;
+  loading?: boolean;
+  loadingIcon?: string;
+  useThrottle?: boolean;
+  throttleDuration?: number;
+}
